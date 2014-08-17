3f70f61c135e869829ef210f24bf99ece1f4866a
the gopher <geosoft1@gmail.com>
2014-08-17 20:03:55 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #
-# golang programming environment installer for ubuntu
+# golang programming environment installer
 # NOTE:
 # this project is third party. for support see http://golang.org and http://code.google.com/p/golangide/
 # author       : geosoft1@gmail.com
 # license      : GPLv3
-# version      : 1.0.0.4
+# version      : 1.0.0.5
 #
-# 07.08.2014   - start project, skeleton, tests (1.0.0.1)
-# 08.08.2014   - added $DESKTOP_SESSION,kernel name and 64bit support (1.0.0.2),eye candy (1.0.0.3)
+# 07.08.2014   - start project, skeleton, tests (1.0.0.1).
+# 08.08.2014   - added $DESKTOP_SESSION,kernel name and 64bit support (1.0.0.2),eye candy (1.0.0.3).
 # 10.08.2014   - better integration with unity launcher bar. compact generated files (1.0.0.4).
+# 17.08.2014   - code restructure (1.0.0.5).
 #
 # TODO:
 # +some Unity2d improvements
 # +shortcut for other desktop environments
-# -adding zenity for more interactivity with the user
-# wget http://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz 2>&1 | sed -u 's/.* \([0-9]%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading go1.3.1.linux-amd64.tar.gz" --auto-close --width="400" 
 # +liteide git support ($HOME/liteide/share/liteide/litebuild/command/go.api)
 #
 
@@ -30,11 +29,13 @@ if [[ $(uname -i) == "i386" ]]; then a="386"; else a="amd64"; fi
 #get kernel name (e.g. linux|freebsd)
 k=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-#build compiler name
-cname=${v}${k}-${a}.tar.gz
+#build compiler name (e.g. go1.3.linux-386.tar.gz)
+n=${v}${k}-${a}.tar.gz
 
-echo "Downloading last compiler $cname..."
-wget -Nq -P $HOME/Downloads http://golang.org/dl/$cname --progress=bar:force 2>&1 | tail -f -n +8
+echo "Download last compiler $n..."
+wget -Nq -P $HOME/Downloads http://golang.org/dl/$n
+echo "Unpack..."
+tar -xf $HOME/Downloads/$n -C $HOME
 
 #get last version of ide (e.g. X23.2)
 v=`echo $(wget -qO- http://sourceforge.net/projects/liteide/files/) | awk '{ if(match($0,/X[0-9]+.[0-9]+/)) print substr($0,RSTART,RLENGTH) }'`
@@ -42,35 +43,36 @@ v=`echo $(wget -qO- http://sourceforge.net/projects/liteide/files/) | awk '{ if(
 #get host computer LONG_BIT (e.g 32|64)
 a=$(getconf LONG_BIT)
 
-#build ide name
-idename=liteidex${v:1}.${k}-${a}.tar.bz2
+#build ide name (e.g. liteidex23.2.linux-32.tar.bz2)
+n=liteidex${v:1}.${k}-${a}.tar.bz2
 
-echo "Downloading last ide $idename..."
-wget -Nq -P $HOME/Downloads http://sourceforge.net/projects/liteide/files/${v}/liteidex${v:1}.${k}-${a}.tar.bz2
+echo "Download last ide $n..."
+wget -Nq -P $HOME/Downloads http://sourceforge.net/projects/liteide/files/${v}/$n
+echo "Unpack..."
+tar -xf $HOME/Downloads/$n -C $HOME
 
-echo "Getting Monaco font..."
+echo "Get Monaco font..."
 wget -Nq -P $HOME/.fonts http://usystem.googlecode.com/files/MONACO.TTF
 
-echo "Unpacking $cname..."
-tar -xf $HOME/Downloads/$cname -C $HOME
+echo "Create \$GOPATH"
+GOPATH=$HOME/go-programs
+mkdir -p $GOPATH/src
 
-echo "Unpacking $idename..."
-tar -xf $HOME/Downloads/$idename -C $HOME
+echo "Add git support to liteide..."
+echo -e "git add
+git commit -m \"-\" -a
+git push" >$HOME/liteide/share/liteide/litebuild/command/go.api
 
-echo "Creating \$GOPATH"
-mkdir -p $HOME/go-programs/src
+#create GOROOT
+GOROOT=$HOME/go
 
+echo "Create liteide.ini.mini"
 #create directory for liteide.ini.mini
 mkdir -p $HOME/.config/liteide
-
-#create directory for liteide.desktop (Unity specific)
-mkdir -p $HOME/.local/share/applications/
-
-echo "Creating liteide.ini.mini"
 echo -e "[liteenv]
-currentenv=linux"$a"
+currentenv=linux$a
 \n[liteide]
-gopath="$HOME"/go-programs
+gopath=$GOPATH
 \n[liteapp]
 geometry=\"@ByteArray(\x1\xd9\xd0\xcb\0\x1\0\0\0\0\0\x41\0\0\0\x18\0\0\x5U\0\0\x2\xff\0\0\0\0\0\0\0,\0\0\x3\x17\0\0\x2\xd3\0\0\0\0\x2\0)\"
 state=@ByteArray(\0\0\0\xff\0\0\0\0\xfd\0\0\0\x4\0\0\0\0\0\0\x1&\0\0\x2\x8d\xfc\x2\0\0\0\x2\xfb\0\0\0\f\0\x64\0o\0\x63\0k\0_\0\x31\0\0\0\0\0\xff\xff\xff\xff\0\0\0 \0\xff\xff\xff\xfb\0\0\0\x18\0\x64\0o\0\x63\0k\0_\0\x31\0_\0s\0p\0l\0i\0t\x1\0\0\0\x39\0\0\x2\x8d\0\0\0\xab\0\xff\xff\xff\0\0\0\x1\0\0\x1\x1d\0\0\x2\x8d\xfc\x2\0\0\0\x2\xfb\0\0\0\f\0\x64\0o\0\x63\0k\0_\0\x32\0\0\0\0\x39\0\0\x2\x8d\0\0\0 \0\xff\xff\xff\xfb\0\0\0\x18\0\x64\0o\0\x63\0k\0_\0\x32\0_\0s\0p\0l\0i\0t\0\0\0\0\0\xff\xff\xff\xff\0\0\0 \0\xff\xff\xff\0\0\0\x2\0\0\0\0\0\0\0\0\xfc\x1\0\0\0\x2\xfb\0\0\0\f\0\x64\0o\0\x63\0k\0_\0\x34\0\0\0\0\0\xff\xff\xff\xff\0\0\0^\0\xff\xff\xff\xfb\0\0\0\x18\0\x64\0o\0\x63\0k\0_\0\x34\0_\0s\0p\0l\0i\0t\0\0\0\0\0\xff\xff\xff\xff\0\0\0^\0\xff\xff\xff\0\0\0\x3\0\0\x4\xdb\0\0\0@\xfc\x1\0\0\0\x2\xfb\0\0\0\f\0\x64\0o\0\x63\0k\0_\0\x38\0\0\0\0\x1d\0\0\x4\xdb\0\0\0^\0\xff\xff\xff\xfb\0\0\0\x18\0\x64\0o\0\x63\0k\0_\0\x38\0_\0s\0p\0l\0i\0t\0\0\0\0\0\xff\xff\xff\xff\0\0\0^\0\xff\xff\xff\0\0\x3\xe9\0\0\x2\x8d\0\0\0\x4\0\0\0\x4\0\0\0\b\0\0\0\b\xfc\0\0\0\x3\0\0\0\0\0\0\0\x1\0\0\0\f\0t\0o\0o\0l\0_\0\x31\x2\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\x1\0\0\0\f\0t\0o\0o\0l\0_\0\x32\x2\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0\0\0\0\x2\0\0\0\x4\0\0\0\f\0t\0o\0o\0l\0_\0\x34\0\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0\0\0\0\x16\0t\0o\0o\0l\0\x62\0\x61\0r\0/\0s\0t\0\x64\0\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0\0\0\0\x16\0t\0o\0o\0l\0\x62\0\x61\0r\0/\0\x65\0n\0v\0\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0\0\0\0\x18\0t\0o\0o\0l\0\x62\0\x61\0r\0/\0t\0\x61\0\x62\0s\x1\0\0\0\0\xff\xff\xff\xff\0\0\0\0\0\0\0\0)
@@ -85,12 +87,12 @@ StartupReloadFiles=true
 StartupReloadFolders=true
 FileWatcherAutoReload=false
 \n[golangdoc]
-goroot="$HOME"/go
+goroot=$GOROOT
 \n[FileBrowser]
-root="$HOME"/go-programs/src
+root=$GOPATH/src
 synceditor=false
 \n[FileManager]
-initpath="$HOME"
+initpath=$HOME
 \n[%General]
 Language=en_US
 WelcomePageVisible=false
@@ -98,26 +100,25 @@ WelcomePageVisible=false
 family=Monaco
 fontsize=12" > $HOME/.config/liteide/liteide.ini.mini
 
-echo "Adding git support to liteide..."
-mv $HOME/liteide/share/liteide/litebuild/command/go.api $HOME/liteide/share/liteide/litebuild/command/go.api.bk
-echo -e "git commit -m \"-\" -a\ngit push\n" >$HOME/liteide/share/liteide/litebuild/command/go.api
-
-#Creating generic .desktop file on desktop
-echo "[Desktop Entry]
+#create generic .desktop file on desktop
+echo -e "[Desktop Entry]
 Version=1.0
 Encoding=UTF-8
 Name=LiteIDE
 Comment=LiteIDE is a simple, open source, cross-platform Go IDE. 
-Exec=sh -c 'cp .config/liteide/liteide.ini.mini .config/liteide/liteide.ini; "$HOME"/liteide/bin/liteide'
-Icon="$HOME"/go/doc/gopher/gophercolor.png
+Exec=sh -c 'cp .config/liteide/liteide.ini.mini .config/liteide/liteide.ini; eval \`ssh-agent -s\`; $HOME/liteide/bin/liteide'
+Icon=$GOROOT/doc/gopher/gophercolor.png
 Type=Application
 Categories=Network;" > $HOME/Desktop/liteide.desktop
 
-echo "Creating smart launcher"
+echo "Create smart launcher"
 case $DESKTOP_SESSION in
 ubuntu*)
+   #create directory for liteide.desktop
+   mkdir -p $HOME/.local/share/applications/
+
    #extend .desktop file with nice options
-   echo -e "\nActions=golang;http server;gopath;
+   echo -e "\nActions=golang;http server;gopath;github;
 \n[Desktop Action golang]
 Name=golang.org
 Exec=firefox golang.org/pkg %U
@@ -129,6 +130,10 @@ OnlyShowIn=Unity;
 \n[Desktop Action gopath]
 Name=\$GOPATH
 Exec=nautilus go-programs/src %U
+OnlyShowIn=Unity;
+\n[Desktop Action github]
+Name=github.com/$GITUSER
+Exec=firefox github.com/$GITUSER %U
 OnlyShowIn=Unity;" >> $HOME/Desktop/liteide.desktop
    #add .desktop file to dash and integrate with unity
    mv $HOME/Desktop/liteide.desktop $HOME/.local/share/applications
@@ -162,11 +167,11 @@ OnlyShowIn=Unity;" >> $HOME/Desktop/liteide.desktop
    ;;
 esac
 
-echo "Creating HelloWorld program"
-mkdir -p $HOME/go-programs/src/HelloWorld
+echo "Create HelloWorld program"
+mkdir -p $GOPATH/src/HelloWorld
 echo -e "package main\n
 func main() {
 	println(\"Hello World!\")
-}" > $HOME/go-programs/src/HelloWorld/main.go
+}" > $GOPATH/src/HelloWorld/main.go
 
 echo "Done."
