ae1cf87f22223dd21f359fa5291e8603b27b3401
geosoft1 <geosoft1@gmail.com>
2015-04-02 11:00:45 +0300

    -

--- Install.sh
+++ Install.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION=1.0.4.3
+VERSION=1.0.4.4
 
 #B0006
 set -e
